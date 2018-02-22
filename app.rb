de400116a3e0cfb8eed8669f348620629d61990d
Blakeley Bond <burrick2003@yahoo.com>
2018-02-22 01:38:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
+  GET '/' do
     erb :index
   end
 
-  get '/new' do
+  GET '/new' do
     erb :create_puppy
   end
 
-  post '/puppy' do
+  POST '/puppy' do
     @name=params['name']
     @breed=params['breed']
     @age=params['age']
